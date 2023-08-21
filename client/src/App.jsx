8c3b7ce725311a58887dccd2c1e07c041750ef49
Ahmer Ali <aliahmer289@gmail.com>
2023-08-21 19:47:10 +0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,194 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import './App.css'; // Create an App.css file for styling
+
+// function App() {
+//   const [url, setUrl] = useState('');
+//   const [qualities] = useState(['highest', 'lowest', 'medium']); // Add your desired qualities
+//   const [selectedQuality, setSelectedQuality] = useState('');
+//   const [downloading, setDownloading] = useState(false);
+//   const [downloadProgress, setDownloadProgress] = useState({ percent: 0, totalSize: 0 });
+
+//   const handleDownload = async () => {
+//     try {
+//       setDownloading(true);
+//       setDownloadProgress({ percent: 0, totalSize: 0 });
+
+//       const response = await axios.post(
+//         'http://localhost:3001/download',
+//         {
+//           url: url,
+//           quality: selectedQuality,
+//         },
+//         {
+//           onDownloadProgress: progressEvent => {
+//             const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+//             setDownloadProgress({ percent: percentCompleted, totalSize: progressEvent.total });
+//           },
+//         }
+//       );
+
+//       // Handle response, e.g., show a download link or success message
+//       console.log('Download response:', response.data);
+//     } catch (error) {
+//       console.error('Error downloading video:', error);
+//       // Handle error, e.g., show an error message
+//     } finally {
+//       setDownloading(false);
+//     }
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>YouTube Video Downloader</h1>
+//       <div className="input-container">
+//         <label>Video URL:</label>
+//         <input
+//           type="text"
+//           value={url}
+//           onChange={(e) => setUrl(e.target.value)}
+//           placeholder="Enter YouTube URL"
+//         />
+//       </div>
+//       <div className="input-container">
+//         <label>Quality:</label>
+//         <select
+//           value={selectedQuality}
+//           onChange={(e) => setSelectedQuality(e.target.value)}
+//         >
+//           {qualities.map((quality, index) => (
+//             <option key={index} value={quality}>{quality}</option>
+//           ))}
+//         </select>
+//       </div>
+//       <button onClick={handleDownload} disabled={downloading}>
+//         {downloading ? 'Downloading...' : 'Download'}
+//       </button>
+//       {downloading && (
+//         <div className="progress-bar">
+//           <div
+//             className="progress"
+//             style={{ width: `${downloadProgress.percent}%` }}
+//           >
+//             {`${downloadProgress.percent}%`}
+//             ({(downloadProgress.totalSize / 1024 / 1024).toFixed(2)} MB)
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'; // Create an App.css file for styling
+import Navbar from './components/Navbar';
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [qualities] = useState(['highest', 'lowest', 'medium']); // Add your desired qualities
+  const [selectedQuality, setSelectedQuality] = useState('');
+  const [downloading, setDownloading] = useState(false);
+  const [downloadProgress, setDownloadProgress] = useState({ percent: 0, totalSize: 0 });
+
+  useEffect(() => {
+    document.body.style.backgroundImage = "url('https://www.useblackbox.io/style/images/bg-shape-006-p-2000.png')";
+  }, []);
+
+  const handleDownload = async () => {
+    try {
+      setDownloading(true);
+      setDownloadProgress({ percent: 0, totalSize: 0 });
+
+      const response = await axios.post(
+        'http://localhost:3001/download',
+        {
+          url: url,
+          quality: selectedQuality,
+        },
+        {
+          onDownloadProgress: progressEvent => {
+            const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+            setDownloadProgress({ percent: percentCompleted, totalSize: progressEvent.total });
+          },
+        }
+      );
+
+      // Handle response, e.g., show a download link or success message
+      console.log('Download response:', response.data);
+    } catch (error) {
+      console.error('Error downloading video:', error);
+      // Handle error, e.g., show an error message
+    } finally {
+      setDownloading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="absolute  right-[100px] z-10 h-[150px] w-[400px] rotate-[0deg] transform rounded-full bg-purple-600 blur-[150px]"></div>
+      <div className="absolute dotted-background h-full top-0 left-0 right-0 z-0">
+        <div className="absolute left-0 right-0 bottom-0 h-[300px]"></div>
+      </div>
+      <div className="relative  container m-auto px-6 md:px-12 lg:px-6">
+        <h1 className="text-slate-200 mt-28 text-center pb-5 text-5xl w-full font-light sm:text-4xl md:text-5xl">
+          YouTube Video{" "}
+          <span className="inline font-bold bg-gradient-to-tr from-orange-500 to-yellow-300 bg-clip-text font-display tracking-tight text-transparent">
+            Downloader
+          </span>
+        </h1>
+        <h2 className="text-neutral-500 mx-auto max-w-screen-sm text-center">
+          Download your favorite videos from YouTube with ease. Enter the video URL and choose the quality.
+        </h2>
+        <hr className="w-96 h-1 mx-auto opacity-20 bg-gray-100 border-0 rounded md:my-8 dark:bg-gray-700" />
+        <div className="input-container ">
+          {/* <label className="text-neutral-500">Video URL:</label> */}
+          <input
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Enter YouTube URL"
+            className="border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
+        {/* </div> */}
+        {/* <div className="input-container"> */}
+          {/* <label className="text-neutral-500">Quality:</label> */}
+          <select
+            value={selectedQuality}
+            onChange={(e) => setSelectedQuality(e.target.value)}
+            className="border border-gray-300 px-3 ml-2 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          >
+            {qualities.map((quality, index) => (
+              <option key={index} value={quality}>{quality}</option>
+            ))}
+          </select>
+        </div>
+        <button
+        onClick={handleDownload}
+        disabled={downloading}
+        className="px-6 py-2 mt-4 bg-gradient-to-tr from-orange-500 to-yellow-300 hover:from-orange-600 hover:to-yellow-400 text-white rounded-md disabled:bg-gray-400 disabled:cursor-not-allowed w-50"
+      >
+        {downloading ? 'Downloading...' : 'Download'}
+      </button>
+        {downloading && (
+          <div className="progress-bar mt-4">
+            <div
+              className="progress bg-blue-500"
+              style={{ width: `${downloadProgress.percent}%` }}
+            >
+              {`${downloadProgress.percent}%`}
+              ({(downloadProgress.totalSize / 1024 / 1024).toFixed(2)} MB)
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,594 @@
+// //Buildin with nodejs
+// const cp = require('child_process');
+// const readline = require('readline');
+// // External modules
+// const ytdl = require('ytdl-core');
+// const ffmpeg = require('ffmpeg-static');
+// // Global constants
+// const ref = 'https://www.youtube.com/watch?v=7zlWwyZApJY&ab_channel=SAMAATV';
+// const tracker = {
+//   start: Date.now(),
+//   audio: { downloaded: 0, total: Infinity },
+//   video: { downloaded: 0, total: Infinity },
+//   merged: { frame: 0, speed: '0x', fps: 0 },
+// };
+
+// // Get audio and video streams
+// const audio = ytdl(ref, { quality: 'highestaudio' })
+//   .on('progress', (_, downloaded, total) => {
+//     tracker.audio = { downloaded, total };
+//   });
+// const video = ytdl(ref, { quality: 'highestvideo' })
+//   .on('progress', (_, downloaded, total) => {
+//     tracker.video = { downloaded, total };
+//   });
+
+// // Prepare the progress bar
+// let progressbarHandle = null;
+// const progressbarInterval = 1000;
+// const showProgress = () => {
+//   readline.cursorTo(process.stdout, 0);
+//   const toMB = i => (i / 1024 / 1024).toFixed(2);
+
+//   process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+//   process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+//   process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+//   process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+//   process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+//   process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+//   process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+//   readline.moveCursor(process.stdout, 0, -3);
+// };
+
+// // Start the ffmpeg child process
+// const ffmpegProcess = cp.spawn(ffmpeg, [
+//   // Remove ffmpeg's console spamming
+//   '-loglevel', '8', '-hide_banner',
+//   // Redirect/Enable progress messages
+//   '-progress', 'pipe:3',
+//   // Set inputs
+//   '-i', 'pipe:4',
+//   '-i', 'pipe:5',
+//   // Map audio & video from streams
+//   '-map', '0:a',
+//   '-map', '1:v',
+//   // Keep encoding
+//   '-c:v', 'copy',
+//   // Define output file
+//   'out.mkv',
+// ], {
+//   windowsHide: true,
+//   stdio: [
+//     /* Standard: stdin, stdout, stderr */
+//     'inherit', 'inherit', 'inherit',
+//     /* Custom: pipe:3, pipe:4, pipe:5 */
+//     'pipe', 'pipe', 'pipe',
+//   ],
+// });
+// ffmpegProcess.on('close', () => {
+//   console.log('done');
+//   // Cleanup
+//   process.stdout.write('\n\n\n\n');
+//   clearInterval(progressbarHandle);
+// });
+
+// // Link streams
+// // FFmpeg creates the transformer streams and we just have to insert / read data
+// ffmpegProcess.stdio[3].on('data', chunk => {
+//   // Start the progress bar
+//   if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+//   // Parse the param=value list returned by ffmpeg
+//   const lines = chunk.toString().trim().split('\n');
+//   const args = {};
+//   for (const l of lines) {
+//     const [key, value] = l.split('=');
+//     args[key.trim()] = value.trim();
+//   }
+//   tracker.merged = args;
+// });
+// audio.pipe(ffmpegProcess.stdio[4]);
+// video.pipe(ffmpegProcess.stdio[5]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const ytdl = require('ytdl-core');
+// const ffmpeg = require('ffmpeg-static');
+// const cp = require('child_process');
+// const readline = require('readline');
+// const cors = require('cors');
+
+// const app = express();
+// const port = 3001;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+
+// // Enable CORS
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+// app.post('/download', (req, res) => {
+//   const url = req.body.url;
+//   const tracker = {
+//     start: Date.now(),
+//     audio: { downloaded: 0, total: Infinity },
+//     video: { downloaded: 0, total: Infinity },
+//     merged: { frame: 0, speed: '0x', fps: 0 },
+//   };
+
+//   const audio = ytdl(url, { quality: 'highestaudio' }).on('progress', (_, downloaded, total) => {
+//     tracker.audio = { downloaded, total };
+//   });
+
+//   const video = ytdl(url, { quality: 'highestvideo' }).on('progress', (_, downloaded, total) => {
+//     tracker.video = { downloaded, total };
+//   });
+
+//   let progressbarHandle = null;
+//   const progressbarInterval = 1000;
+
+//   const showProgress = () => {
+//     readline.cursorTo(process.stdout, 0);
+//     const toMB = i => (i / 1024 / 1024).toFixed(2);
+
+//     process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+//     process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+//     process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+//     process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+//     readline.moveCursor(process.stdout, 0, -3);
+//   };
+
+//   const ffmpegProcess = cp.spawn(ffmpeg, [
+//     '-loglevel', '8', '-hide_banner',
+//     '-progress', 'pipe:3',
+//     '-i', 'pipe:4',
+//     '-i', 'pipe:5',
+//     '-map', '0:a',
+//     '-map', '1:v',
+//     '-c:v', 'copy',
+//     'out.mkv',
+//   ], {
+//     windowsHide: true,
+//     stdio: ['inherit', 'inherit', 'inherit', 'pipe', 'pipe', 'pipe'],
+//   });
+
+//   ffmpegProcess.on('close', () => {
+//     console.log('done');
+//     process.stdout.write('\n\n\n\n');
+//     clearInterval(progressbarHandle);
+//   });
+
+//   ffmpegProcess.stdio[3].on('data', chunk => {
+//     if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+//     const lines = chunk.toString().trim().split('\n');
+//     const args = {};
+//     for (const l of lines) {
+//       const [key, value] = l.split('=');
+//       args[key.trim()] = value.trim();
+//     }
+//     tracker.merged = args;
+//   });
+
+//   audio.pipe(ffmpegProcess.stdio[4]);
+//   video.pipe(ffmpegProcess.stdio[5]);
+
+//   res.sendStatus(200);
+// });
+
+// app.listen(port, '10.135.84.136', () => {
+//   console.log(`Server is listening on 10.135.84.136:${port}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const ytdl = require('ytdl-core');
+// const ffmpeg = require('ffmpeg-static');
+// const cp = require('child_process');
+// const readline = require('readline');
+// const cors = require('cors');
+// const { v4: uuidv4 } = require('uuid'); // Import the uuid package
+
+// const app = express();
+// const port = 3001;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+
+// // Enable CORS
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+// app.post('/download', (req, res) => {
+//   const url = req.body.url;
+//   const tracker = {
+//     start: Date.now(),
+//     audio: { downloaded: 0, total: Infinity },
+//     video: { downloaded: 0, total: Infinity },
+//     merged: { frame: 0, speed: '0x', fps: 0 },
+//   };
+
+//   const audio = ytdl(url, { quality: 'highestaudio' }).on('progress', (_, downloaded, total) => {
+//     tracker.audio = { downloaded, total };
+//   });
+
+//   const video = ytdl(url, { quality: 'highestvideo' }).on('progress', (_, downloaded, total) => {
+//     tracker.video = { downloaded, total };
+//   });
+
+//   let progressbarHandle = null;
+//   const progressbarInterval = 1000;
+
+//   const showProgress = () => {
+//     readline.cursorTo(process.stdout, 0);
+//     const toMB = i => (i / 1024 / 1024).toFixed(2);
+
+//     process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+//     process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+//     process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+//     process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+//     readline.moveCursor(process.stdout, 0, -3);
+//   };
+
+//   const filename = `${uuidv4()}.mkv`; // Generate a random filename
+//   const ffmpegProcess = cp.spawn(ffmpeg, [
+//     '-loglevel', '8', '-hide_banner',
+//     '-progress', 'pipe:3',
+//     '-i', 'pipe:4',
+//     '-i', 'pipe:5',
+//     '-map', '0:a',
+//     '-map', '1:v',
+//     '-c:v', 'copy',
+//     filename, // Use the generated filename
+//   ], {
+//     windowsHide: true,
+//     stdio: ['inherit', 'inherit', 'inherit', 'pipe', 'pipe', 'pipe'],
+//   });
+
+//   ffmpegProcess.on('close', () => {
+//     console.log('done');
+//     process.stdout.write('\n\n\n\n');
+//     clearInterval(progressbarHandle);
+//   });
+
+//   ffmpegProcess.stdio[3].on('data', chunk => {
+//     if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+//     const lines = chunk.toString().trim().split('\n');
+//     const args = {};
+//     for (const l of lines) {
+//       const [key, value] = l.split('=');
+//       args[key.trim()] = value.trim();
+//     }
+//     tracker.merged = args;
+//   });
+
+//   audio.pipe(ffmpegProcess.stdio[4]);
+//   video.pipe(ffmpegProcess.stdio[5]);
+
+//   res.sendStatus(200);
+// });
+
+// app.listen(port, '10.135.84.136', () => {
+//   console.log(`Server is listening on 10.135.84.136:${port}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//working
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const ytdl = require('ytdl-core');
+// const ffmpeg = require('ffmpeg-static');
+// const cp = require('child_process');
+// const readline = require('readline');
+// const cors = require('cors');
+// const { v4: uuidv4 } = require('uuid'); // Import the uuid package
+
+// const app = express();
+// const port = 3001;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+
+// // Enable CORS
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+// app.post('/download', (req, res) => {
+//   const url = req.body.url;
+//   const quality = req.body.quality; // Get the selected quality
+
+//   const tracker = {
+//     start: Date.now(),
+//     audio: { downloaded: 0, total: Infinity },
+//     video: { downloaded: 0, total: Infinity },
+//     merged: { frame: 0, speed: '0x', fps: 0 },
+//   };
+
+//   const audio = ytdl(url, { quality: `${quality}audio` }).on('progress', (_, downloaded, total) => {
+//     tracker.audio = { downloaded, total };
+//   });
+
+//   const video = ytdl(url, { quality: `${quality}video` }).on('progress', (_, downloaded, total) => {
+//     tracker.video = { downloaded, total };
+//   });
+
+//   let progressbarHandle = null;
+//   const progressbarInterval = 1000;
+
+//   const showProgress = () => {
+//     readline.cursorTo(process.stdout, 0);
+//     const toMB = i => (i / 1024 / 1024).toFixed(2);
+
+//     process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+//     process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+//     process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+//     process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+//     process.stdout.write(`Running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+//     readline.moveCursor(process.stdout, 0, -3);
+//   };
+
+//   const filename = `${uuidv4()}.mkv`; // Generate a random filename
+//   const ffmpegProcess = cp.spawn(ffmpeg, [
+//     '-loglevel', '8', '-hide_banner',
+//     '-progress', 'pipe:3',
+//     '-i', 'pipe:4',
+//     '-i', 'pipe:5',
+//     '-map', '0:a',
+//     '-map', '1:v',
+//     '-c:v', 'copy',
+//     filename, // Use the generated filename
+//   ], {
+//     windowsHide: true,
+//     stdio: ['inherit', 'inherit', 'inherit', 'pipe', 'pipe', 'pipe'],
+//   });
+
+//   ffmpegProcess.on('close', () => {
+//     console.log('done');
+//     process.stdout.write('\n\n\n\n');
+//     clearInterval(progressbarHandle);
+//   });
+
+//   ffmpegProcess.stdio[3].on('data', chunk => {
+//     if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+//     const lines = chunk.toString().trim().split('\n');
+//     const args = {};
+//     for (const l of lines) {
+//       const [key, value] = l.split('=');
+//       args[key.trim()] = value.trim();
+//     }
+//     tracker.merged = args;
+//   });
+
+//   audio.pipe(ffmpegProcess.stdio[4]);
+//   video.pipe(ffmpegProcess.stdio[5]);
+
+//   res.sendStatus(200);
+// });
+
+// app.listen(port, '10.135.84.199', () => {
+//   console.log(`Server is listening on 10.135.84.199:${port}`);
+// });
+
+ 
+
+
+
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg-static');
+const cp = require('child_process');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const readline = require('readline');
+const os = require('os');
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.post('/download', (req, res) => {
+  const url = req.body.url;
+  const quality = req.body.quality;
+
+  const tracker = {
+    start: Date.now(),
+    audio: { downloaded: 0, total: Infinity },
+    video: { downloaded: 0, total: Infinity },
+    merged: { frame: 0, speed: '0x', fps: 0 },
+  };
+
+  const audio = ytdl(url, { quality: `${quality}audio` }).on('progress', (_, downloaded, total) => {
+    tracker.audio = { downloaded, total };
+  });
+
+  const video = ytdl(url, { quality: `${quality}video` }).on('progress', (_, downloaded, total) => {
+    tracker.video = { downloaded, total };
+  });
+
+  let progressbarHandle = null;
+  const progressbarInterval = 1000;
+
+  const showProgress = () => {
+    readline.cursorTo(process.stdout, 0);
+    const toMB = i => (i / 1024 / 1024).toFixed(2);
+
+    process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+    process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+    process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+    process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+    process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+    process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+    process.stdout.write(`Running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+    readline.moveCursor(process.stdout, 0, -3);
+  };
+
+  const outputFilePath = path.join(os.homedir(), 'Downloads', `${uuidv4()}.mkv`);
+  const ffmpegProcess = cp.spawn(ffmpeg, [
+    '-loglevel', '8', '-hide_banner',
+    '-progress', 'pipe:3',
+    '-i', 'pipe:4',
+    '-i', 'pipe:5',
+    '-map', '0:a',
+    '-map', '1:v',
+    '-c:v', 'copy',
+    outputFilePath,
+  ], {
+    windowsHide: true,
+    stdio: ['inherit', 'inherit', 'inherit', 'pipe', 'pipe', 'pipe'],
+  });
+
+  ffmpegProcess.on('close', () => {
+    console.log('done');
+    process.stdout.write('\n\n\n\n');
+    clearInterval(progressbarHandle);
+
+    res.status(200).json({ message: 'Video downloaded successfully.' });
+  });
+
+  ffmpegProcess.stdio[3].on('data', chunk => {
+    if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+    const lines = chunk.toString().trim().split('\n');
+    const args = {};
+    for (const l of lines) {
+      const [key, value] = l.split('=');
+      args[key.trim()] = value.trim();
+    }
+    tracker.merged = args;
+  });
+
+  audio.pipe(ffmpegProcess.stdio[4]);
+  video.pipe(ffmpegProcess.stdio[5]);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on :${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
